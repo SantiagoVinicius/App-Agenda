58777362bc6308be0e8563035a12bec805050a23
SantiagoVinicius <viniciussantiagosantos19@gmail.com>
2022-06-22 22:14:04 -0300

    agenda de contatos

--- AppAgenda/www/js/funcoes.js
+++ AppAgenda/www/js/funcoes.js
@@ -1,15 +1,28 @@
 function SalvarAnotacao(){
-    novoContato = document.getElementById("nome").value
-    novoTel = document.getElementById("tel").value
-    document.getElementById("anotacoes-salvas").innerHTML += "<p>" +
-    "Nome: " + novoContato + " " + "Telefone: " + novoTel + "</p>"
+   let novoNome = document.getElementById("nome").value
+   let novoTel = document.getElementById("tel").value
+    let novoContato = `<p id="contato" >${novoNome} - ${novoTel}</p>`
+    document.getElementById("salvar").innerHTML += novoContato
     document.getElementById("nome").value = " " 
     document.getElementById("tel").value = " " 
   
-
+    localStorage.salvar = novoContato
 }
 
 document.getElementById("botao-salvar").addEventListener("click", function(){
     SalvarAnotacao()
     navigator.vibrate(7000);
 })
+
+if(localStorage.salvar){
+    document.getElementById("salvar").innerHTML = localStorage.salvar;
+}
+
+
+
+apagar = document.getElementById("apagar");
+apagar.addEventListener("click", function(){
+    localStorage.clear();
+    document.getElementById("contato").remove();
+})
+
